e5ad437fde69a0fb6fd8580918e6d234ff59e068
Mad-Max98 <massimiliano.giovanni.colombo@gmail.com>
2023-09-13 18:49:52 +0200

    update carousel

---
+++ acf-blocks/carousel/carousel.php
@@ -0,0 +1,15 @@
+<?php 
+
+$images = get_field('acf_gallery');
+
+$size = 'full'; // (thumbnail, medium, large, full or custom size)
+
+if( $images ): ?>
+    <ul>
+        <?php foreach( $images as $image ): ?>
+            <li>
+                <?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
